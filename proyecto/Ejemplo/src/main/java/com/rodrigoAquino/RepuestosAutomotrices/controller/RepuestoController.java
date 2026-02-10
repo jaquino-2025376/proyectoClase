52fec256761b6c525ec4177d5fd6d8d51f244898
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-10 11:10:26 -0600

    Clase: Controller de Repuesto terminado

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/controller/RepuestoController.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/controller/RepuestoController.java
@@ -1,4 +1,56 @@
 package com.rodrigoAquino.RepuestosAutomotrices.controller;
 
+import com.rodrigoAquino.RepuestosAutomotrices.model.Repuesto;
+import com.rodrigoAquino.RepuestosAutomotrices.service.RepuestoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/repuestos")
+
 public class RepuestoController {
+    private final RepuestoService repuestoService;
+
+    public RepuestoController(RepuestoService repuestoService) {
+        this.repuestoService = repuestoService;
+    }
+
+    @GetMapping
+    public List<Repuesto> getAllRepuestos() {
+        return repuestoService.getAllRepuestos();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createRepuesto(@Valid @RequestBody Repuesto repuesto) {
+        try {
+            Repuesto createdRepuesto = repuestoService.saveRepuesto(repuesto);
+            return new ResponseEntity<>(createdRepuesto, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateRepuesto(@PathVariable Integer id, @Valid @RequestBody Repuesto repuesto) {
+        try {
+            Repuesto updateRepuesto = repuestoService.updateRepuesto(id, repuesto);
+            return ResponseEntity.ok(updateRepuesto);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteRepuesto(@PathVariable Integer id) {
+        try {
+            repuestoService.deleteRepuesto(id);
+            return ResponseEntity.ok("El repuesto fue eliminado con éxito: " + id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo eliminar el proveedor: " + e.getMessage());
+        }
+    }
 }
