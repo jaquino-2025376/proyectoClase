324b3d9703999f2b0f97b46876bd4a49f73871dc
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-10 08:43:46 -0600

    Modelo de proveedor completo

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/model/Proveedor.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/model/Proveedor.java
@@ -1,4 +1,67 @@
 package com.rodrigoAquino.RepuestosAutomotrices.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Proveedores")
 public class Proveedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_proveedor")
+    private Integer idProveedor;
+
+    @Column(name = "nombre_proveedor")
+    private String nombreProveedor;
+
+    @Column(name = "telefono_proveedor")
+    private Integer telefonoProveedor;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "email_proveedor")
+    private String emailProveedor;
+
+    //Getter y setters
+
+
+    public Integer getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(Integer idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public Integer getTelefonoProveedor() {
+        return telefonoProveedor;
+    }
+
+    public void setTelefonoProveedor(Integer telefonoProveedor) {
+        this.telefonoProveedor = telefonoProveedor;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmailProveedor() {
+        return emailProveedor;
+    }
+
+    public void setEmailProveedor(String emailProveedor) {
+        this.emailProveedor = emailProveedor;
+    }
 }
