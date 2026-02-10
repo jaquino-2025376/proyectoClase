2c4717e8cdcbfcabd81d249361e8746b3deeb506
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-10 10:26:56 -0600

    Clase: Repuesto Service clase terminada

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/RepuestoService.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/RepuestoService.java
@@ -1,4 +1,16 @@
 package com.rodrigoAquino.RepuestosAutomotrices.service;
 
+import com.rodrigoAquino.RepuestosAutomotrices.model.Proveedor;
+import com.rodrigoAquino.RepuestosAutomotrices.model.Repuesto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface RepuestoService {
+    List<Repuesto> getAllRepuestos();
+    Repuesto getRepuestoById(Integer Id);
+    Repuesto saveRepuesto (Repuesto repuesto) throws RuntimeException;
+    Repuesto updateRepuesto(Integer id, Repuesto repuesto);
+    void deleteRepuesto(Integer id);
 }
