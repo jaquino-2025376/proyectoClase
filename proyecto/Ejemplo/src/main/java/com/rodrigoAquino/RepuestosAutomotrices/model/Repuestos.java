9b4b9b8672ec0c3dd3f4f8c21044c653bc6117b1
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-10 10:14:28 -0600

    Modelo de repuestos terminado

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/model/Repuestos.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/model/Repuestos.java
@@ -1,4 +1,81 @@
 package com.rodrigoAquino.RepuestosAutomotrices.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Repuestos")
 public class Repuestos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_repuesto")
+    private Integer idRepuesto;
+
+    @Column(name = "nombre_repuesto")
+    private String nombreRepuesto;
+
+    @Column(name = "categoria_repuesto")
+    private String categoriaRepuesto;
+
+    @Column(name = "precio_compra")
+    private Float precioCompra;
+
+    @Column(name = "precio_venta")
+    private Float precioVenta;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_proveedor")
+    private Integer idProveedor;
+
+    //Getter y setter
+
+
+    public Integer getIdRepuesto() {
+        return idRepuesto;
+    }
+
+    public void setIdRepuesto(Integer idRepuesto) {
+        this.idRepuesto = idRepuesto;
+    }
+
+    public String getNombreRepuesto() {
+        return nombreRepuesto;
+    }
+
+    public void setNombreRepuesto(String nombreRepuesto) {
+        this.nombreRepuesto = nombreRepuesto;
+    }
+
+    public String getCategoriaRepuesto() {
+        return categoriaRepuesto;
+    }
+
+    public void setCategoriaRepuesto(String categoriaRepuesto) {
+        this.categoriaRepuesto = categoriaRepuesto;
+    }
+
+    public Float getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(Float precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public Float getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Float precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+    
+
+    public Integer getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(Integer idProveedor) {
+        this.idProveedor = idProveedor;
+    }
 }
