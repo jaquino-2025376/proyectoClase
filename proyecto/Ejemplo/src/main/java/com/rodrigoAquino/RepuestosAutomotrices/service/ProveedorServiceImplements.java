11a926e0b27626ee2c66781a085a166f72fa9aa3
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-10 09:15:21 -0600

    Proveedor Service Implements creado completo

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/ProveedorServiceImplements.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/ProveedorServiceImplements.java
@@ -1,4 +1,50 @@
 package com.rodrigoAquino.RepuestosAutomotrices.service;
 
-public class ProveedorServiceImplements {
+import com.rodrigoAquino.RepuestosAutomotrices.model.Empleado;
+import com.rodrigoAquino.RepuestosAutomotrices.model.Proveedor;
+import com.rodrigoAquino.RepuestosAutomotrices.repository.ProveedorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProveedorServiceImplements implements ProveedorService {
+    private final ProveedorRepository proveedorRepository;
+
+    public ProveedorServiceImplements(ProveedorRepository proveedorRepository) {
+        this.proveedorRepository = proveedorRepository;
+    }
+
+    @Override
+    public List<Proveedor> getAllProveedores() {
+        return proveedorRepository.findAll();
+    }
+
+    @Override
+    public Proveedor getProveedorById(Integer Id) {
+        return proveedorRepository.findById(Id).orElse(null);
+    }
+
+    @Override
+    public Proveedor saveProveedor(Proveedor proveedor) throws RuntimeException {
+        return proveedorRepository.save(proveedor);
+    }
+
+    @Override
+    public Proveedor updateProveedor(Integer id, Proveedor proveedorDetalles) {
+        Proveedor proveedorExistente = proveedorRepository.findById(id).orElse(null);
+        if (proveedorExistente != null) {
+            proveedorExistente.setNombreProveedor(proveedorDetalles.getNombreProveedor());
+            proveedorExistente.setTelefonoProveedor(proveedorDetalles.getTelefonoProveedor());
+            proveedorExistente.setDireccion(proveedorDetalles.getDireccion());
+            proveedorExistente.setEmailProveedor(proveedorDetalles.getEmailProveedor());
+            return proveedorRepository.save(proveedorExistente);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteProveedor(Integer id) {
+        proveedorRepository.deleteById(id);
+    }
 }
