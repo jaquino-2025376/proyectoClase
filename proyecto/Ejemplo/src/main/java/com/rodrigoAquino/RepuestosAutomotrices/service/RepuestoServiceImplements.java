c3b2e37c440e95a09dc8c549059d7fcdf2ed3c4d
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-10 10:52:42 -0600

    Clase: el Service Implements de repuesto esta completo

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/RepuestoServiceImplements.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/RepuestoServiceImplements.java
@@ -1,4 +1,50 @@
 package com.rodrigoAquino.RepuestosAutomotrices.service;
 
-public class RepuestoServiceImplements {
+import com.rodrigoAquino.RepuestosAutomotrices.model.Repuesto;
+import com.rodrigoAquino.RepuestosAutomotrices.repository.RepuestoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RepuestoServiceImplements implements RepuestoService  {
+    private final RepuestoRepository repuestoRepository;
+
+    public RepuestoServiceImplements(RepuestoRepository repuestoRepository) {
+        this.repuestoRepository = repuestoRepository;
+    }
+
+    @Override
+    public List<Repuesto> getAllRepuestos() {
+        return repuestoRepository.findAll();
+    }
+
+    @Override
+    public Repuesto getRepuestoById(Integer Id){
+        return repuestoRepository.findById(Id).orElse(null);
+    }
+
+    @Override
+    public Repuesto saveRepuesto(Repuesto repuesto) throws RuntimeException{
+        return repuestoRepository.save(repuesto);
+    }
+
+    @Override
+    public Repuesto updateRepuesto(Integer id, Repuesto repuestoDetalles) {
+        Repuesto repuestoExistente = repuestoRepository.findById(id).orElse(null);
+        if (repuestoExistente != null){
+            repuestoExistente.setNombreRepuesto(repuestoDetalles.getNombreRepuesto());
+            repuestoExistente.setCategoriaRepuesto(repuestoDetalles.getCategoriaRepuesto());
+            repuestoExistente.setPrecioCompra(repuestoDetalles.getPrecioCompra());
+            repuestoExistente.setIdProveedor(repuestoDetalles.getIdProveedor());
+            return repuestoRepository.save(repuestoExistente);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteRepuesto(Integer id) {
+        repuestoRepository.deleteById(id);
+    }
+
 }
