e8254a75d7fab489c0080062a1d12c951704e080
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-10 10:44:09 -0600

    Error: Se corrigió un error en la clase Repuesto Repository

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/RepuestoRepository.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/RepuestoRepository.java
@@ -1,4 +1,9 @@
 package com.rodrigoAquino.RepuestosAutomotrices.repository;
 
-public interface RepuestoRepository {
+import com.rodrigoAquino.RepuestosAutomotrices.model.Repuesto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepuestoRepository extends JpaRepository<Repuesto, Integer> {
 }
