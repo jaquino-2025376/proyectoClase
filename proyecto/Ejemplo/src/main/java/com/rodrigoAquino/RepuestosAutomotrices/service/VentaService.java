10cd02aef0306fa859743c26d0bec1823c7801f0
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-11 08:53:26 -0600

    Clase: Servicio de ventas terminado

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/VentaService.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/VentaService.java
@@ -1,4 +1,14 @@
 package com.rodrigoAquino.RepuestosAutomotrices.service;
 
+import com.rodrigoAquino.RepuestosAutomotrices.model.Venta;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public interface VentaService {
+    List<Venta> getAllVentas();
+    Venta getVentaById(Integer id);
+    Venta saveVenta(Venta venta) throws RuntimeException;
+    Venta updateVenta(Integer id, Venta venta);
+    void deleteVenta(Integer id)
 }
