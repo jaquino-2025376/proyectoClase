1efd2df82c6566f2a99641c5fb6c06efe24726be
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-11 08:44:13 -0600

    Clase: clase modelo venta terminada

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/model/Venta.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/model/Venta.java
@@ -1,4 +1,80 @@
 package com.rodrigoAquino.RepuestosAutomotrices.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Ventas")
 public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_venta")
+    private Integer idVenta;
+
+    @Column(name = "fecha_venta")
+    private java.time.LocalDate fechaVenta;
+
+    @Column(name = "cantidad")
+    private Integer cantidad;
+
+    @Column(name = "total")
+    private Float total;
+
+    @Column(name = "id_empleado")
+    private Integer idEmpleado;
+
+    @Column(name = "id_repuesto")
+    private Integer idRepuesto;
+
+    //Getter y Setter
+
+
+    public Integer getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Integer idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public LocalDate getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Integer getIdRepuesto() {
+        return idRepuesto;
+    }
+
+    public void setIdRepuesto(Integer idRepuesto) {
+        this.idRepuesto = idRepuesto;
+    }
 }
