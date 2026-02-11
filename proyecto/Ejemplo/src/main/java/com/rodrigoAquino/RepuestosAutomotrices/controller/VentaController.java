85d803c44ba07abca0dab336a358c00dc46dd08e
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-11 09:28:00 -0600

    Clase: controller de ventas terminado

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/controller/VentaController.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/controller/VentaController.java
@@ -1,4 +1,52 @@
 package com.rodrigoAquino.RepuestosAutomotrices.controller;
 
+import com.rodrigoAquino.RepuestosAutomotrices.model.Venta;
+import com.rodrigoAquino.RepuestosAutomotrices.service.VentaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ventas")
+
 public class VentaController {
+    private final VentaService ventaService;
+
+    public VentaController(VentaService ventaService){this.ventaService = ventaService;}
+
+    @GetMapping
+    public List<Venta> getAllVentas(){return ventaService.getAllVentas();}
+
+    @PostMapping
+    public ResponseEntity<Object> createVenta(@Valid @RequestBody Venta venta){
+        try {
+            Venta createdVenta = ventaService.saveVenta(venta);
+            return new ResponseEntity<>(createdVenta, HttpStatus.CREATED);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateVenta(@PathVariable Integer id, @Valid @RequestBody Venta venta){
+        try{
+            Venta updateVenta = ventaService.updateVenta(id, venta);
+            return ResponseEntity.ok(updateVenta);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteVenta(@PathVariable Integer id){
+        try{
+            ventaService.deleteVenta(id);
+            return ResponseEntity.ok("La venta fue eliminada con éxito: " + id);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo eliminar la venta: " + e.getMessage());
+        }
+    }
 }
