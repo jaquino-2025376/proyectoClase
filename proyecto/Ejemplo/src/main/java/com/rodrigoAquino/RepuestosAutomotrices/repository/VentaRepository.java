2e1abfb3e54447f17c28dca9ff27058009fa7e5f
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-11 08:46:44 -0600

    Clase: clase repository de ventas terminada

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/VentaRepository.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/VentaRepository.java
@@ -1,4 +1,9 @@
 package com.rodrigoAquino.RepuestosAutomotrices.repository;
 
-public interface VentaRepository {
+import com.rodrigoAquino.RepuestosAutomotrices.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Integer> {
 }
