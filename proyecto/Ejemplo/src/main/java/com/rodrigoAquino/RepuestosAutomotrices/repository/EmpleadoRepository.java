7435c0e262489d7c71bb6db9c5794b5d2ef03df7
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-13 14:23:52 -0600

    Optional creado para manejo de validacion para no crear un empleado con el mismo nombre y apellido

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/EmpleadoRepository.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/EmpleadoRepository.java
@@ -4,7 +4,10 @@ import com.rodrigoAquino.RepuestosAutomotrices.model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
 
+    Optional<Empleado> findByNombreEmpleadoAndApellidoEmpleado(String nombreEmpleado, String apellidoEmpleado);
 }
