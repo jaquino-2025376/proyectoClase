1567aeedf0ffe426e5e680d8b609b12f8cabcd4c
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-13 15:10:33 -0600

    Optional añadido para manejo de valores

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/ProveedorRepository.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/ProveedorRepository.java
@@ -4,6 +4,9 @@ import com.rodrigoAquino.RepuestosAutomotrices.model.Proveedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProveedorRepository extends JpaRepository<Proveedor, Integer> {
+    Optional<Proveedor> findByNombreAndEmailProveedor(String nombreProveedor, String direccion);
 }
