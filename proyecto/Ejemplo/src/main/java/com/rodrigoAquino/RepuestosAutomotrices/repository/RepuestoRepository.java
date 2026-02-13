c8b37f468d3a47fd0ad0b67e5893204bb08fef75
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-13 15:44:35 -0600

    Repository con optional para manejo de valores y validacion

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/RepuestoRepository.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/RepuestoRepository.java
@@ -4,6 +4,9 @@ import com.rodrigoAquino.RepuestosAutomotrices.model.Repuesto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RepuestoRepository extends JpaRepository<Repuesto, Integer> {
+    Optional<Repuesto> findByNombreRepuesto(String nombreRepuesto);
 }
