51457808063fd95497976bead52428d8fb09fb68
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-13 19:41:46 -0600

    Método creado en venta repository

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/VentaRepository.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/repository/VentaRepository.java
@@ -1,9 +1,14 @@
 package com.rodrigoAquino.RepuestosAutomotrices.repository;
 
+import com.rodrigoAquino.RepuestosAutomotrices.model.Empleado;
+import com.rodrigoAquino.RepuestosAutomotrices.model.Repuesto;
 import com.rodrigoAquino.RepuestosAutomotrices.model.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VentaRepository extends JpaRepository<Venta, Integer> {
+    Optional<Venta> findByIdEmpleadoAndIdRepuesto(Integer idEmpleado, Integer idRepuesto);
 }
