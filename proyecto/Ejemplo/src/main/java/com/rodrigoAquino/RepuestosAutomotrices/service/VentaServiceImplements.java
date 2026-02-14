feb488dde67f8bd64166e358831403c46f1bd1ca
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-02-13 19:44:58 -0600

    validaciones creadas e implementadas en el service implements

--- proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/VentaServiceImplements.java
+++ proyecto/Ejemplo/src/main/java/com/rodrigoAquino/RepuestosAutomotrices/service/VentaServiceImplements.java
@@ -1,17 +1,30 @@
 package com.rodrigoAquino.RepuestosAutomotrices.service;
 
 import com.rodrigoAquino.RepuestosAutomotrices.model.Venta;
+import com.rodrigoAquino.RepuestosAutomotrices.model.Empleado;
+import com.rodrigoAquino.RepuestosAutomotrices.model.Repuesto;
 import com.rodrigoAquino.RepuestosAutomotrices.repository.VentaRepository;
+import com.rodrigoAquino.RepuestosAutomotrices.repository.EmpleadoRepository;
+import com.rodrigoAquino.RepuestosAutomotrices.repository.RepuestoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class VentaServiceImplements implements VentaService{
-    private final VentaRepository ventaRepository;
+public class VentaServiceImplements implements VentaService {
 
-    public VentaServiceImplements(VentaRepository ventaRepository){
+    private final VentaRepository ventaRepository;
+    private final EmpleadoRepository empleadoRepository;
+    private final RepuestoRepository repuestoRepository;
+
+    public VentaServiceImplements(
+            VentaRepository ventaRepository,
+            EmpleadoRepository empleadoRepository,
+            RepuestoRepository repuestoRepository) {
+
         this.ventaRepository = ventaRepository;
+        this.empleadoRepository = empleadoRepository;
+        this.repuestoRepository = repuestoRepository;
     }
 
     @Override
@@ -21,29 +34,41 @@ public class VentaServiceImplements implements VentaService{
 
     @Override
     public Venta getVentaById(Integer id) {
-        return ventaRepository.findById(id).orElse(null);
+        return ventaRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Venta no encontrada"));
     }
 
     @Override
-    public Venta saveVenta(Venta venta) throws RuntimeException {
+    public Venta saveVenta(Venta venta) {
+        empleadoRepository.findById(venta.getIdEmpleado()).orElseThrow(() -> new IllegalArgumentException("Empleado no encontrado"));
+        repuestoRepository.findById(venta.getIdRepuesto()).orElseThrow(() -> new IllegalArgumentException("Repuesto no encontrado"));
         return ventaRepository.save(venta);
     }
 
+
+
+
     @Override
-    public Venta updateVenta(Integer id, Venta ventaDetalles){
-        Venta ventaExistente = ventaRepository.findById(id).orElse(null);
-        if (ventaExistente != null){
-            ventaExistente.setFechaVenta(ventaDetalles.getFechaVenta());
-            ventaExistente.setCantidad(ventaDetalles.getCantidad());
-            ventaExistente.setTotal(ventaDetalles.getTotal());
-            ventaExistente.setIdEmpleado(ventaDetalles.getIdEmpleado());
-            ventaExistente.setIdRepuesto(ventaDetalles.getIdRepuesto());
-        }
-        return null;
+    public Venta updateVenta(Integer id, Venta ventaDetalles) {
+        Venta ventaExistente = ventaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Venta no encontrada"));
+        Empleado empleado = empleadoRepository.findById(ventaDetalles.getIdEmpleado()).orElseThrow(() -> new IllegalArgumentException("Empleado no encontrado"));
+
+        Repuesto repuesto = repuestoRepository.findById(ventaDetalles.getIdRepuesto()).orElseThrow(() -> new IllegalArgumentException("Repuesto no encontrado"));
+        ventaExistente.setFechaVenta(ventaDetalles.getFechaVenta());
+        ventaExistente.setCantidad(ventaDetalles.getCantidad());
+        ventaExistente.setTotal(ventaDetalles.getTotal());
+        ventaExistente.setIdEmpleado(empleado.getIdEmpleado());
+        ventaExistente.setIdRepuesto(repuesto.getIdRepuesto());
+
+
+        return ventaRepository.save(ventaExistente);
     }
 
     @Override
     public void deleteVenta(Integer id) {
-    ventaRepository.deleteById(id);
+        Venta venta = ventaRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Venta no encontrada"));
+
+        ventaRepository.delete(venta);
     }
 }
